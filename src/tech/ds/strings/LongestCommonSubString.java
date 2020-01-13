f155846dfa17a051b41aeea5cefc1f8c6382c5b0
rameshkatiyar <er.rameshkatiyar@gmail.com>
2020-01-13 23:53:27 +0530

    Did practice of Longest palindromic substring program via  O(N) Manacher's Algorithm.

---
+++ src/tech/ds/strings/LongestCommonSubString.java
@@ -0,0 +1,21 @@
+package tech.ds.strings;
+
+/**
+ * Dynamic Programming
+ *
+ * Formula:
+ *         if (input1[i] == input2[j]){
+ *              T[i][j] = T[i-1][j-1]+1
+ *         }else {
+ *              T[i][j] = 0; //IMP
+ *         }
+ *
+ * Complexity: O(mn) time and space complexity
+ *
+ * Video:https://www.youtube.com/watch?v=BysNXJHzCEs
+ */
+public class LongestCommonSubString {
+    public static void main(String args[]){
+
+    }
+}
